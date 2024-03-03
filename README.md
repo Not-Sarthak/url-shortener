b16529c9e514bb45de0e24241a027cc5c0fd1e6f
Sarthak <notsarthakshah@gmail.com>
2024-03-03 06:53:08 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# url-shortener
+# shorty
+
+Shorty is a simple URL shortener built with nodejs
+
+<img src="./assets/shorty.png" alt="Screen">
+
+## How to setup ?
+
+```
+$ npm install
+```
+
+## How to start ?
+
+```
+$ npm start
+```
---
+++ static/script.js
@@ -0,0 +1,42 @@
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const info = document.querySelector("#info");
+
+function getShortedLink(id) {
+  return document.location.href + "short/" + id;
+}
+
+function displayLink(link) {
+  info.style.color = "var(--color)";
+  info.style.display = "block";
+  info.textContent = link + "\n(Copied to clipboard)";
+}
+
+function displayError(err) {
+  info.style.color = "var(--error)";
+  info.style.display = "block";
+  info.textContent = err || "Error";
+}
+
+btn.addEventListener("click", async function () {
+  try {
+    const url = input.value;
+
+    if (
+      url.trim() === "" ||
+      !/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6}).+/.test(url)
+    ) {
+      throw new Error("Invalid URL");
+    }
+
+    const req = await fetch("/add/" + encodeURIComponent(url));
+    const rep = await req.json();
+    if (rep.status === "OK") {
+      const link = getShortedLink(rep.data.id);
+      navigator.clipboard.writeText(link);
+      displayLink(link);
+    }
+  } catch (e) {
+    displayError(e);
+  }
+});
